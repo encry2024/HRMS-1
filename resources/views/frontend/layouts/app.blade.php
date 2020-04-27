c317c9da73a523614887c295c284f6b8fe1a4124
Kim Gumahin <kgumahin@stratpoint.com>
2020-04-27 17:35:08 +0800

    added layout for frontend and backend

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -3,8 +3,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-4.4.1/dist/css/bootstrap.min.css') }}">
 </head>
 <body>
+    <script src= "{{ asset ('javascript/jquery.min.js') }}"> </script>
     
 </body>
 </html>
